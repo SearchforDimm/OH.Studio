3f736da76c924709756b69fccc56afc9d8ff8ac2
Dimm <dmzrfqi@gmail.com>
2024-02-27 16:32:28 +0700

    Initialization

---
+++ oh.studio/script.js
@@ -0,0 +1,12 @@
+// $("#myvid").hover(function() {
+//     $(this).prop("controls", true);
+// }, function() {
+//     $(this).prop("controls", false);
+// });
+
+// $("#myvid").click(function() {
+//     if(this.paused)
+//     this.play();
+//     else
+//     this.pause();
+// })
